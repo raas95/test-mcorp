be782226c24cfcf6ba358ac79f129eb573d7df56
rafi <rafiahmadassidiq@gmail.com>
2024-11-03 07:18:03 +0700

    fixing color stat

--- src/components/PokemonDetail/PokemonStats.tsx
+++ src/components/PokemonDetail/PokemonStats.tsx
@@ -3,13 +3,13 @@ import React from "react";
 const PokemonStats = ({ stats }: { stats: any[] }) => {
     return (
         <div className="mb-8">
-            <h2 className="text-2xl font-semibold mb-4">Base Stats</h2>
+            <h2 className="text-2xl font-semibold mb-4 text-white">Base Stats</h2>
             <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
                 {stats.map((stat: any) => (
                     <div key={stat.stat.name}>
                         <div className="flex justify-between border-b border-gray-300 pb-1">
-                            <span className="capitalize">{stat.stat.name.replace('-', ' ')}</span>
-                            <span className="font-semibold">{stat.base_stat}</span>
+                            <span className="capitalize text-white">{stat.stat.name.replace('-', ' ')}</span>
+                            <span className="font-semibold text-white">{stat.base_stat}</span>
                         </div>
                         <div
                             className="bg-blue-500 h-2 rounded-full"
